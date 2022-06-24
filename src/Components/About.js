059dae2fe405eb1a14c4c3f7189324ce4ce3c151
skkyfallen <kenzemadrt@gmail.com>
2022-06-24 16:29:41 +0100

    Update

---
+++ src/Components/About.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./About.css";
+const About = () => {
+  return (
+    <div>
+      <section className="About-Me">
+        <h1>About Me</h1>
+        <p>
+          My full name is Ekwonu Kenechukwu but my online persona is <br />
+          "skkyfallen". I am a Front End Developer and a Video editor.
+          <br />
+          I am a full time student at Babcock University Ilishan Ogun
+          <br />
+          Currently studying Software Engineering since 2019. I Enjoy <br />
+          building responsive websites with ReactJs and also editing
+          <br />
+          videos for different purposes. My full Portfolio is below.
+        </p>
+      </section>
+    </div>
+  );
+};
+
+export default About;
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,13 +2,14 @@ import React from 'react'
 import Navbar from './Navbar.js'
 import Landing from './Landing.js'
 import "./Home.css"
+import About from "./About.js"
 
 const Home = () => {
   return (
     <div>
   <Navbar/>
   <Landing/>
-
+  <About/>
 
 
         
